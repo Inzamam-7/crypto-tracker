52369a7e39bfe3025a4a484b052f17ecadf8c797
Inzamamul Haque Ata <inzamamulhaqueata@gmail.com>
2025-10-29 12:35:57 +0530

    Fix import path for CoinDetails component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
-import CoinDetails from './pages/CoinDetails'
+import CoinDetails from './pages/coinDetails'
 
 const App = () => {
   return (
